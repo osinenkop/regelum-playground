9ef7c1b338aa5a50fd5f4ca0bdb333fa47cbeba7
Anton <bolychev.anton@gmail.com>
2024-03-27 21:22:00 +0300

    feat(configs): add configs

--- src/policy.py
+++ src/policy.py
@@ -2,9 +2,34 @@ from regelum.policy import Policy
 import numpy as np
 
 
-class MyPolicy(Policy):
-    def __init__(self):
+class InvPendulumPolicyPID(Policy):
+    def __init__(self, pd_coefs, action_min, action_max):
         super().__init__()
 
-    def get_action(self, observation):
-        return np.array([[6, 2]])
+        self.pid_coefs = np.array(pd_coefs).reshape(1, -1)
+        self.action_min = action_min
+        self.action_max = action_max
+
+    def get_action(self, observation: np.ndarray):
+        action = np.clip(
+            (self.pid_coefs * observation).sum(),
+            self.action_min,
+            self.action_max,
+        )
+        return np.array([[action]])
+
+
+class InvPendulumPolicyEnergyBased(Policy):
+    def __inti__(self, action_min, action_max):
+        super().__init__()
+
+        # saving action bounds
+        self.action_min = action_min
+        self.action_max = action_max
+
+    def get_action(self, observation: np.ndarray) -> np.ndarray:
+        action = np.clip(
+            (np.array([-10, -10]) * observation).sum(),
+            self.action_min,
+            self.action_max,
+        )
