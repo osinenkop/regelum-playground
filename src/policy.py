c3b05021906ec1a85def4350bdcfa312c1a0060b
osinenkop <p.osinenko@yandex.ru>
2024-04-08 14:34:07 +0300

    A bit of renaming for clarity

--- src/policy.py
+++ src/policy.py
@@ -78,7 +78,7 @@ class InvertedPendulumEnergyBased(Policy):
 
         energy_total = (
             mass * grav_const * length * (np.cos(angle) - 1) / 2
-            + 0.5 * self.system.pendulum_moment() * angle_vel**2
+            + 0.5 * self.system.pendulum_moment_inertia() * angle_vel**2
         )
         energy_control_action = -self.gain * np.sign(angle_vel * energy_total)
 
@@ -134,11 +134,11 @@ class InvPendulumEnergyBasedFrictionCompensation(Policy):
         angle_vel = observation[0, 1]
         energy_total = (
             mass * grav_const * length * (np.cos(angle) - 1) / 2
-            + 0.5 * self.system.pendulum_moment() * angle_vel**2
+            + 0.5 * self.system.pendulum_moment_inertia() * angle_vel**2
         )
         energy_control_action = -self.gain * np.sign(
             angle_vel * energy_total
-        ) + friction_coeff * self.system.pendulum_moment() * angle_vel * np.abs(
+        ) + friction_coeff * self.system.pendulum_moment_inertia() * angle_vel * np.abs(
             angle_vel
         )
 
@@ -200,11 +200,11 @@ class InvPendulumEnergyBasedFrictionAdaptive(Policy):
 
         energy_total = (
             mass * grav_const * length * (np.cos(angle) - 1) / 2
-            + 0.5 * self.system.pendulum_moment() * angle_vel**2
+            + 0.5 * self.system.pendulum_moment_inertia() * angle_vel**2
         )
         energy_control_action = -self.gain * np.sign(
             angle_vel * energy_total
-        ) + self.friction_coeff_est * self.system.pendulum_moment() * angle_vel * np.abs(
+        ) + self.friction_coeff_est * self.system.pendulum_moment_inertia() * angle_vel * np.abs(
             angle_vel
         )
 
--- src/system.py
+++ src/system.py
@@ -7,7 +7,7 @@ class InvertedPendulum(InvertedPendulum):
 
     _parameters = {"mass": 0.127, "grav_const": 9.81, "length": 0.337}
 
-    def pendulum_moment(self):
+    def pendulum_moment_inertia(self):
         return self._parameters["mass"] * self._parameters["length"] ** 2 / 3
 
     def _compute_state_dynamics(self, time, state, inputs):
@@ -24,7 +24,7 @@ class InvertedPendulum(InvertedPendulum):
         Dstate[0] = state[1]
         Dstate[1] = (
             grav_const * mass * length * rg.sin(state[0]) / 2 + inputs[0]
-        ) / self.pendulum_moment()
+        ) / self.pendulum_moment_inertia()
 
         return Dstate
 
@@ -55,7 +55,7 @@ class InvertedPendulumWithFriction(InvertedPendulum):
         Dstate[0] = state[1]
         Dstate[1] = (
             grav_const * mass * length * rg.sin(state[0]) / 2 + inputs[0]
-        ) / self.pendulum_moment() - friction_coeff * state[1] ** 2 * rg.sign(state[1])
+        ) / self.pendulum_moment_inertia() - friction_coeff * state[1] ** 2 * rg.sign(state[1])
 
         return Dstate
 
@@ -67,9 +67,9 @@ class InvertedPendulumWithMotor(InvertedPendulum):
         "mass": 0.127,
         "grav_const": 9.81,
         "length": 0.337,
-        "tau_motor": 0.05,
-        "m_motor": 0.1,
-        "r_motor": 0.04,
+        "motor_time_const": 0.05,
+        "motor_mass": 0.1,
+        "motor_radius": 0.04,
     }
     _dim_state = 3
     _dim_observation = 3
@@ -84,7 +84,7 @@ class InvertedPendulumWithMotor(InvertedPendulum):
     _action_bounds = [[-1.0, 1.0]]
 
     def motor_moment(self):
-        return self._parameters["m_motor"] * self._parameters["r_motor"] ** 2 / 2
+        return self._parameters["motor_mass"] * self._parameters["motor_radius"] ** 2 / 2
 
     def _compute_state_dynamics(self, time, state, inputs):
         Dstate = rg.zeros(
@@ -92,17 +92,17 @@ class InvertedPendulumWithMotor(InvertedPendulum):
             prototype=(state, inputs),
         )
 
-        mass, grav_const, length, tau_motor = (
+        mass, grav_const, length, motor_time_const = (
             self._parameters["mass"],
             self._parameters["grav_const"],
             self._parameters["length"],
-            self._parameters["tau_motor"],
+            self._parameters["motor_time_const"],
         )
 
         Dstate[0] = state[1]
         Dstate[1] = (mass * grav_const * length * rg.sin(state[0]) / 2 + state[2]) / (
-            self.pendulum_moment() + self.motor_moment()
+            self.pendulum_moment_inertia() + self.motor_moment()
         )
-        Dstate[2] = (inputs[0] - state[2]) / tau_motor
+        Dstate[2] = (inputs[0] - state[2]) / motor_time_const
 
         return Dstate
